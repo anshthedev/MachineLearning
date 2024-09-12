10873553838105e189b9bc582ae5096be239320b
Ansh <anshkool1104@gmail.com>
2024-09-12 10:02:14 -0500

    Fixed Combining Dataset Bugs
    There was a logic error when trying to add a combined dataset into each participant's folder.

--- ResearchDataset.py
+++ ResearchDataset.py
@@ -13,22 +13,14 @@ for filename in os.listdir(directory):
         for filename2 in os.listdir(f):
             f2 = os.path.join(f, filename2)
 
-            print(filename2)
 
             if os.path.isdir(f2):
                 for filename3 in os.listdir(f2):
                     f3 = os.path.join(f2, filename3)
 
-                    print(filename3)
-
                     if filename3 == 'TEMP.csv':
                         temp = pd.read_csv(f3)
                         df1 = pd.DataFrame(temp)
-                        # temp_data.columns = [f'{col}_{filename}' for col in temp_data.columns]
-                        #
-                        # # Reset the index to avoid index issues and concatenate horizontally
-                        # temp_data = temp_data.reset_index(drop=True)
-                        #
 
                         if joined_data.empty:
                             joined_data = df1
@@ -36,6 +28,7 @@ for filename in os.listdir(directory):
                             joined_data = pd.concat([joined_data.reset_index(drop=True), df1.reset_index(drop=True)],
                                                     axis=1)
 
+    if os.path.isdir(f):
+        joined_data.to_csv(f+'/CombinedTemp.csv')
 
-joined_data.to_csv(f+'/CombinedTemp.csv')
 
