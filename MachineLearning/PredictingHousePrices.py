49f411a393890c308aaea7daeabd511e3729658f
anshthedev <anshkool1104@gmail.com>
2024-10-18 10:49:07 -0500

    Trying RandomForest and DecisionTrees

--- MachineLearning/PredictingHousePrices.py
+++ MachineLearning/PredictingHousePrices.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
@@ -20,6 +21,22 @@ train_x, val_x, train_y, val_y = train_test_split(x, y, random_state = 0)
 
 melbourne_model = DecisionTreeRegressor(random_state = 1)
 
-melbourne_model.fit(train_x, train_y)
 
-print(mean_absolute_error(val_y, melbourne_model.predict(val_x)))
+def get_mae_tree(max_leaf_nodes, train_x, train_y, val_x, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_x, train_y)
+
+    return mean_absolute_error(val_y, model.predict(val_x))
+
+def get_mae_forest(train_x, train_y, val_x, val_y):
+    model = RandomForestRegressor(random_state = 1)
+    model.fit(train_x, train_y)
+
+    return mean_absolute_error(val_y, model.predict(val_x))
+
+for max_leaf_nodes in [800]:
+    mae = get_mae_tree(max_leaf_nodes, train_x, train_y, val_x, val_y)
+    print((max_leaf_nodes, mae))
+    print(get_mae_forest(train_x, train_y, val_x, val_y))
+
+
